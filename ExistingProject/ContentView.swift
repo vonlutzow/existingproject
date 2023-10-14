688b148b89d4949b0b36bcea5fc083452abd5ca5
vonlutzow <vonlutzow@gmail.com>
2023-10-14 07:10:56 -0400

    Initial Commit

---
+++ ExistingProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExistingProject
+//
+//  Created by Todd Perkins on 11/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExistingProject/ExistingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExistingProjectApp.swift
+//  ExistingProject
+//
+//  Created by Todd Perkins on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct ExistingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
