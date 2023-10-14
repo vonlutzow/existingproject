ce81b3cf352917ee3ad8be8647dd0e48d4298ac1
vonlutzow <vonlutzow@gmail.com>
2023-10-14 07:28:31 -0400

    new text

--- ExistingProject/ContentView.swift
+++ ExistingProject/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world! Xcode 12 is great")
             .padding()
             .font(.title)
     }
